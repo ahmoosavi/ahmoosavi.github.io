1b4edadfa8526b04e280a4c42e3dc169385c4b61
ahmoosavi <82632526+ahmoosavi@users.noreply.github.com>
2025-07-21 02:26:34 +0000

    Deploying to gh-pages from @ ahmoosavi/ahmoosavi.github.io@b517ae21ef40b9531339cf219cc92ccd5523fbb7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -398,16 +398,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-our-work-on-center-wise-disparities-in-kidney-post-transplant-outcomes-was-accepted-for-oral-presentation-at-the-2025-world-transplant-congress",
+          title: 'Our work on center-wise disparities in kidney post-transplant outcomes was accepted for oral...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
