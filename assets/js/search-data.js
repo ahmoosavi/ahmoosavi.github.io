28dc3417b50b859736114fed8221695f896d051f
ahmoosavi <82632526+ahmoosavi@users.noreply.github.com>
2025-07-21 02:27:43 +0000

    Deploying to gh-pages from @ ahmoosavi/ahmoosavi.github.io@41a1c4785e61bf13238aa75f8f5acfd49c51d8f6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -398,18 +398,18 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
           section: "News",},{id: "news-our-work-on-center-wise-disparities-in-kidney-post-transplant-outcomes-was-accepted-for-oral-presentation-at-the-2025-world-transplant-congress",
           title: 'Our work on center-wise disparities in kidney post-transplant outcomes was accepted for oral...',
           description: "",
-          section: "News",},{id: "projects-project-1",
+          section: "News",},{id: "news-a-long-announcement-with-details",
+          title: 'A long announcement with details',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/announcement_2/";
+            },},{id: "projects-project-1",
           title: 'project 1',
           description: "with background image",
           section: "Projects",handler: () => {
